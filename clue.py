58b06537ab217cd7d620cdf25fdf8790bf1e43cf
sergionl <sergio.nl0803@gmail.com>
2021-11-24 18:29:04 -0500

    eliminado archivos sin uso

--- clue.py
+++ clue.py
@@ -1,79 +1,196 @@
 from tkinter import *
+from PIL import ImageTk, Image
 from utils import *
-
+from tkinter import messagebox
 root = Tk()
-root.title("TF-Cluedo")
+kbs=[createKb(),createKb()]
+lcards = cartas() #list cards
+wcards = selectedCartasGanador() #winner cards
+available_cards = list()
+available_cards = initial_available_cards()
 
-Label(root, text="Cluedo").grid(row=0, column=1, columnspan=3)
-Label(root, text="1) Hoy a sido asesinado ..."+"\n"+
+class Player():
+    def __init__(self, txt,i):
+        self.window = Toplevel()
+        self.window.geometry("600x300")
+        self.title = txt
+        self.i=i
+        self.cards = [(-1, -1) for _ in range(3)]
+        self.checked = IntVar()
+        self.button=Button()
+        Label(self.window, text=self.title).pack()
+        self.entry = Entry(self.window, borderwidth=2)
+        self.show = Checkbutton(self.window, text="Mostrar cartas", variable=self.checked, command=self.showcards)
+        self.buttonknowledge= Button(self.window, text="Mostrar conocimiento",command= self.showknowledge)
+        self.buttonpick = Button(self.window, text="Tomar una carta",command= self.pickCard)
+        self.buttonAcusar=Button(self.window,text="Acusar",command= self.acusar)
+        self.fcards = LabelFrame()
+
+        self.entry.pack()
+        self.show.pack()
+        self.buttonknowledge.pack()
+        self.buttonpick.pack()
+        self.buttonAcusar.pack()
+    
+    def selectInitialCards(self, AnotherPlayerCards, i):
+        self.cards, kbs[i] = selectCartasIniciales(wcards, AnotherPlayerCards, kbs[i])
+        return 
+
+    def showknowledge(self):
+        kbs[self.i],mensajes=helpSecurity(kbs[self.i],lcards)
+        messagebox.showinfo("Respuestas",mensajes)
+        return
+        
+    def showcards(self):
+        
+        if(self.checked.get() == 1):
+            self.fcards.pack_forget()
+            self.canshow = False
+
+            self.fcards = LabelFrame(self.window, text="Cards " + self.title)
+            self.fcards.pack(padx=10, pady=10)
+            
+            for i,j in self.cards:
+                Label(self.fcards, text=lcards[i][j]).pack()
+        else:
+            self.fcards.pack_forget()
+
+    def pickCard(self):
+        global available_cards
+        if len(available_cards)==0:
+            messagebox.showerror("Error","No hay mas cartas")
+            return
+        card = selectCarta(available_cards)
+        
+        self.cards.append(card)
+        kbs[self.i] = changeValue(kbs[self.i],card,False)
+        
+        available_cards = quit_some_availablecards(available_cards, [card])
+
+        self.showcards()
+        self.buttonpick['state'] = DISABLED
+
+    def acusar(self):
+        global new_img
+        ventanaAcusar=Toplevel()
+        ventanaAcusar.title("Acusar")
+        ventanaAcusar.geometry("300x400")
+        Label(ventanaAcusar,text="Acusar "+str(self.i+1)).pack()
+        
+        canva = Canvas(ventanaAcusar, width=150, height=200)
+        canva.pack()
+        my_img = (Image.open("./img/acusar.jpg"))
+        resized_img = my_img.resize((150, 200), Image.ANTIALIAS)
+        new_img = ImageTk.PhotoImage(resized_img)
+        canva.create_image(10, 10, anchor=NW, image=new_img)
+        
+        Label(ventanaAcusar,text="Persona: ").pack()
+        entryPersona=Entry(ventanaAcusar)
+        entryPersona.pack()
+        
+        Label(ventanaAcusar,text="Lugar: ").pack()
+        entryLugar=Entry(ventanaAcusar)
+        entryLugar.pack()
+        
+        Label(ventanaAcusar,text="Motivo: ").pack()
+        entryMotivo=Entry(ventanaAcusar)
+        entryMotivo.pack()
+        def boton(state : bool):
+            
+            return
+        def acusacion():
+            numeroPersona=nameToNumber(entryPersona.get())
+            numeroMotivo=nameToNumber(entryMotivo.get())
+            numeroLugar=nameToNumber(entryLugar.get())
+            if numeroPersona == (-1,-1):
+                messagebox.showwarning("Error","No existe esta persona")
+                return
+            if numeroMotivo == (-1,-1):
+                messagebox.showwarning("Error","No hemos tomado en cuenta este motivo para el juego")
+                return
+            if numeroLugar == (-1,-1):
+                messagebox.showwarning("Error","No existe este lugar")
+                return
+            mensaje=verification(numeroPersona,numeroLugar,numeroMotivo,wcards)
+            
+            messagebox.showinfo("anuncio",mensaje)
+            if mensaje== "Ganaste":
+                root.destroy()
+            return
+        Button(ventanaAcusar, text="Enviar",command=acusacion).pack()
+        Button(ventanaAcusar, text="Retroceder",command=ventanaAcusar.destroy).pack()
+        
+        return
+    
+def play(pregunta:Player,respuesta:Player,i):
+    texto=pregunta.entry.get()
+    numero=nameToNumber(texto)
+    if numero == (-1,-1):
+        messagebox.showwarning("Error","Debes ingresar una carta correcta")
+        
+        return
+    kbs[i],mensaje=question(kbs[i],numero[0],numero[1],respuesta.cards)
+    pregunta.button['state'] = DISABLED
+    messagebox.showinfo("Base de conocimiento",mensaje)       
+    return 
+def showAnotherplayer(window1: Toplevel, window2: Toplevel, player: Player):
+    window1.withdraw()
+    window2.deiconify()
+
+    player.buttonpick['state'] = NORMAL
+    player.button['state'] = NORMAL
+
+def start():
+    global root, available_cards, wcards
+    root.withdraw()
+    player0 = Player("Jugador 1",0)
+    player1 = Player("Jugador 2",1)    
+
+    player0.selectInitialCards(player1.cards, 0)
+    player1.selectInitialCards(player0.cards, 1)
+
+    Button(player0.window, text="Enviar", command= lambda:play(player0,player1,0)).pack()
+    Button(player0.window, text="El siguiente jugador", 
+    command=lambda: showAnotherplayer(player0.window, player1.window, player0)).pack()
+    Button(player1.window, text="Enviar", command= lambda:play(player1,player0,1)).pack()
+    Button(player1.window, text="El siguiente jugador", 
+    command=lambda: showAnotherplayer(player1.window, player0.window, player1)).pack()
+
+    player1.window.withdraw()
+
+    available_cards = quit_some_availablecards(available_cards, player0.cards)
+    available_cards = quit_some_availablecards(available_cards, player1.cards)
+    available_cards = quit_some_availablecards(available_cards, wcards)
+
+
+def instructions():
+    global new_img
+
+    root.title("TF-Cluedo")
+    lbl = Label(root, text="Cluedo", border=3, padx=5, pady=5)
+    lbl.pack(anchor=CENTER)
+    lbl.config(fg="white",
+               bg="black",
+               font=("Verdana", 24, "bold"))
+    
+    canva = Canvas(root, width=150, height=200)
+    canva.pack()
+    my_img = (Image.open("./img/cluedo.jpg"))
+    resized_img = my_img.resize((150, 200), Image.ANTIALIAS)
+    new_img = ImageTk.PhotoImage(resized_img)
+    canva.create_image(10, 10, anchor=NW, image=new_img)
+
+    
+    lbl = Label(root, text="1) Hoy a sido asesinado ..."+"\n"+
     "2) Debes encontrar ¿Quién lo hizo? ¿Cual fue el motivo? ¿Y donde?"+"\n"+
     "3) Se esconderá tres cartas (persona, motivo, lugar) sobre el asesinato"+"\n"+
     "4) El resto de cartas se repartiran entre los 2 jugadores"+"\n"+
-    "4) Deberás encontrar las cartas escondidas", justify="left").grid(row=1, column=2, rowspan=2, columnspan=2)
+    "4) Deberás encontrar las cartas escondidas", justify="left", padx=10, pady=5)
+    lbl.pack()
+    lbl.config(font=("Verdana", 10))
 
-#######################################################################
-#Modelo
-Example = createKb()
-
-#######################################################################
-
-rows = [10, 10]
-cols = [2, 3]
-
-kbs=[createKb(),createKb()]
-def start ():
-    print(type(kbs[0]))
-    lcartas=cartas()
-    cartasGanadores=selectedCartasGanador(lcartas)
-    cartasJ1,kbs[0]=selectCartasJugador(lcartas,cartasGanadores,[-1,-1,-1],kbs[0])
-    Label(root,text=str(cartasJ1[0])).grid(row=rows[0], column=cols[0])
-    rows[0]+=1
-    Label(root,text=str(cartasJ1[1])).grid(row=rows[0], column=cols[0])
-    rows[0]+=1
-    Label(root,text=str(cartasJ1[2])).grid(row=rows[0], column=cols[0])
-    rows[0]+=1
-    cartasJ2,kbs[1]=selectCartasJugador(lcartas,cartasGanadores,cartasJ1,kbs[1])
-    Label(root,text=str(cartasJ2[0])).grid(row=rows[1], column=cols[1])
-    rows[1]+=1
-    Label(root,text=str(cartasJ2[1])).grid(row=rows[1], column=cols[1])
-    rows[1]+=1
-    Label(root,text=str(cartasJ2[2])).grid(row=rows[1], column=cols[1])
-    rows[1]+=1
-    
-def click_Player(txt, i):
-    cardType, cardName=nameToNumber(txt)
-    if cardType != -1:
-        print(cardType, cardName)
-    else:
-        print("Ingrese valores correctos")
-    Label(root, text=txt).grid(row=rows[i], column=cols[i])
-    rows[i] += 1
+    Button(root, text="Comenzar (>)", bg="green", fg="black", pady=10, command=start).pack()
 
 
-player1 = Label(root, text="Jugador 1")
-player1.grid(row=4, column=2)
-
-EntryPlayer1 = Entry(root, borderwidth=2)
-EntryPlayer1.grid(row=6, column=2)
-
-EnviarPlayer1 = Button(root, text="Enviar", command=lambda: click_Player(EntryPlayer1.get(), 0))
-EnviarPlayer1.grid(row=7, column=2)
-
-ConcimientoPlayer1 = Button(root, text="Mostrar conocimiento")
-ConcimientoPlayer1.grid(row=8, column=2)
-
-#######################################################################
-
-jugador2 = Label(root, text="Jugador 2")
-jugador2.grid(row=4, column=3)
-
-EntryPlayer2 = Entry(root, borderwidth=2)
-EntryPlayer2.grid(row=6, column=3)
-
-ButtonPlayer2 = Button(root, text="Enviar", command=lambda: click_Player(EntryPlayer2.get(), 1))
-ButtonPlayer2.grid(row=7, column=3)
-
-ConcimientoPlayer2 = Button(root, text="Mostrar conocimiento")
-ConcimientoPlayer2.grid(row=8, column=3)
-start()
+instructions()
 root.mainloop()
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
+from typing import AsyncIterable
 import numpy as np
 import random
 def cartas():
+    """
+    Define las cartas del juego y las guarda en una matriz de 6x3.
+    """
     cartasCulpable=['Mora','Blanco','Amapola','Prado','Rubio','Celeste']
     cartasLugar=['Patio','Estudio','Comedor','Garaje','Salon','Dormitorio']
     cartasMotivo=['Racismo','Homofobia','Transfobia','Xenofobia','Sexismo','Discriminacion por discapacidad']
@@ -9,36 +13,70 @@ def cartas():
     return cartas
 
 def createKb():
+    """
+    Se crea una base de conocimiento, esta tiene forma de una matriz de 3x6, ya que esa son la cantidad de cartas en el juego.
+
+    La base de conocimiento tiene datos iniciales en -1, ya que este representa falta de informacion.
+    """
     kb = np.zeros((3,6))
     for i in range(18):
         kb.itemset(i,-1)
-    #print(kb)
     return kb
 
-def changeValue(kb,cardType,card,info):
+def changeValue(kb, card, info):
+    """
+    Cambia los valores a la base de conocimiento.
+
+    0 representa que esa carta no es la respuesta.
+
+    1 representa que esa carta es parte de la respuesta.
+    """
+
     if info:
-        kb.itemset((cardType,card),1)
+        kb.itemset(card, 1)
     else:
-        kb.itemset((cardType,card),0)
+        kb.itemset(card, 0)
     return kb
 
-def seeSecurity(kb,cardType):
+def seeSecurity(kb,cardType,cartas):
+    """
+    Muestra si nuestra base de conocimiento tiene certeza sobre algun tipo de cartas.
+    Se basa en la regla solo una carta de cada tipo es la respuesta correcta, esto se puede entender como:
+
+    -Solo existe una carta de culpables que es el culpable:  ∃x. (CartaCulpable(x) ∧ Culpable(x))
+
+    -Solo existe una carta de lugares que es el lugar:  ∃x. (CartaLugar(x) ∧ Lugar(x))
+
+    -Solo existe una carta de motivos que es el motivo:  ∃x. (CartaMotivo(x) ∧ Motivo(x))
+    
+    """
     count0 = 0
     for i in range(6):
         if kb[cardType,i] == 0:
             count0 +=1
         if kb[cardType,i] == 1:
-            print("Carta {0} es parte de la respuesta".format(cartas[cardType][i]))
-            return kb
+            mensaje="Carta {0} es parte de la respuesta".format(cartas[cardType][i])
+            return kb,mensaje
     if count0 == 5:
         for i in range(6):
             if kb[cardType,i] == -1:
-                print("Carta {0} es parte de la respuesta".format(cartas[cardType][i]))
-                kb = changeValue(kb,cardType,i,True)
-                return kb
-    print("La base de conocimiento no tiene informacion suficiente")
+                kb = changeValue(kb,(cardType,i),True)
+                mensaje="Carta {0} es parte de la respuesta".format(cartas[cardType][i])
+                return kb,mensaje
+    mensaje="La base de conocimiento no tiene informacion suficiente sobre "
+    if cardType==0:
+        mensaje+="Los culpables"
+    elif cardType==1:
+        mensaje+="El lugar"
+    else:
+        mensaje+="El motivo" 
+    return kb,mensaje
 
 def nameToNumber(name):
+    """
+    Recive la carta seleccionada por el usuario y devuelve 
+    la posicion de la matriz que representa esa carta.
+    """
     name = name.lower()
     cardType,card = -1,-1
     if name == "mora":
@@ -60,86 +98,150 @@ def nameToNumber(name):
         cardType,card = 0,5
         return cardType,card
     elif name == "racismo":
-        cardType,card = 1,0
-        return cardType,card
-    elif name == "homofobia":
-        cardType,card = 1,1
-        return cardType,card
-    elif name == "transfobia":
-        cardType,card = 1,2
-        return cardType,card
-    elif name == "xenofobia":
-        cardType,card = 1,3
-        return cardType,card
-    elif name == "sexismo":
-        cardType,card = 1,4
-        return cardType,card
-    elif name == "discriminacion por discapacidad":
-        cardType,card = 1,5
-        return cardType,card
-    elif name == "patio":
         cardType,card = 2,0
         return cardType,card
-    elif name == "estudio":
+    elif name == "homofobia":
         cardType,card = 2,1
         return cardType,card
-    elif name == "comedor":
+    elif name == "transfobia":
         cardType,card = 2,2
         return cardType,card
-    elif name == "garaje":
+    elif name == "xenofobia":
         cardType,card = 2,3
         return cardType,card
-    elif name == "salon":
+    elif name == "sexismo":
         cardType,card = 2,4
         return cardType,card
-    elif name == "dormitorio":
+    elif name == "discriminacion por discapacidad":
         cardType,card = 2,5
         return cardType,card
+    elif name == "patio":
+        cardType,card = 1,0
+        return cardType,card
+    elif name == "estudio":
+        cardType,card = 1,1
+        return cardType,card
+    elif name == "comedor":
+        cardType,card = 1,2
+        return cardType,card
+    elif name == "garaje":
+        cardType,card = 1,3
+        return cardType,card
+    elif name == "salon":
+        cardType,card = 1,4
+        return cardType,card
+    elif name == "dormitorio":
+        cardType,card = 1,5
+        return cardType,card
     else:
-        print("Ingrese una carta existente")
+        return -1,-1
 
-def helpSecurity(kb):
+def helpSecurity(kb,cartas):
+    """
+    Función auxiliar
+
+    LLama 3 veces a la función seeSecurity para revisar si se tiene certeza de algun tipo de cartas
+    """
+    mensajes=""
     for i in range(3):
-        kb = seeSecurity(kb,i)
-    return kb
+        kb,aux = seeSecurity(kb,i,cartas)
+        mensajes+=aux+"\n"
+    return kb, mensajes
 
-def selectedCartasGanador(cartas):
-    carta1=random.randint(0,5)
-    carta2=random.randint(0,5)
-    carta3=random.randint(0,5)
-    print(carta1,carta2,carta3)
-    return [carta1,carta2,carta3]
+def selectedCartasGanador():
+    """
+    Selecciona aleatoriamente las cartas del ganador
+    """
+    number1=random.randint(0,5)
+    number2=random.randint(0,5)
+    number3=random.randint(0,5)
+    return [(0, number1), (1, number2), (2, number3)]
 
-def selectCartasJugador(cartas,ganador,otrojugador,kb):
-    while True:
-        carta1= random.randint(0,5)
-        if carta1== ganador[0]:
-            continue
-        if carta1==otrojugador[0]:
-            continue
-        kb=changeValue(kb,0,carta1,False)
-        break
-    while True:
-        carta2= random.randint(0,5)
-        if carta2== ganador[1]:
-            continue
-        if carta2==otrojugador[1]:
-            continue
-        kb=changeValue(kb,1,carta2,False)
-        break
-    while True:
-        carta3= random.randint(0,5)
-        if carta3== ganador[2]:
-            continue
-        if carta3==otrojugador[2]:
-            continue
-        kb=changeValue(kb,2,carta3,False)
-        break
-    return [carta1, carta2, carta3], kb
+def selectCartasIniciales(ganador, otrojugador, kb):
+    """
+    Para iniciar el juego se reparte cartas, la funcion se encarga de seleccionar cartas aleatoriamente a 
+    los jugadores y seleccionar aleatoriamente las cartas ganadore(culpable, motivo y lugar)
+    """
+    selected = []
+    
+    for i in range(3):
+        while True:
+            number = random.randint(0,5)
+            if number == ganador[i][1]:
+                continue
+
+            if (number == otrojugador[i][1]):
+                continue
+            
+            card = (i, number)
+            selected.append(card)
+            kb=changeValue(kb, card, False)
+
+            break
+
+    return selected, kb
+
+def selectCarta(availablecards):
+    """
+    Selecciona la carta que solicita
+    """
+    ans = (-1, -1)
+    if len(availablecards) == 0:
+        return ans
+
+    return random.choice(availablecards)
+
+def initial_available_cards():
+    """
+    Se crea la lista inicial de cartas
+    """
+    lista = list()
+    for i in range(3):
+        for j in range(6):
+            lista.append((i, j))
+    
+    return lista
+
+def quit_some_availablecards(available: list, group: list):
+    """
+    Quita la carta que el jugador robo en su turno de la lista de cartas que aun no roba
+    """
+    for element in group:
+        if element in available:
+            available.remove(element)
+    
+    return available
 
 def question(kb,cardType,card,otroJugador):
-    if otroJugador[cardType] == card:
-        kb = changeValue(kb,cardType,card,False)
+    """
+    Revisa si el jugador y tiene la respuesta a la pregunta de jugador x, y llama a la funcion changeValue
+
+    """
+    
+    if (cardType,card) in otroJugador:
+        mensaje="Base de conocimiento actualizada"
+        kb = changeValue(kb,(cardType,card),False)
     else:
-        print("No se obtiene respuesta")
-    return kb
+        mensaje="No se obtiene respuesta"
+    return kb,mensaje
+
+def verification(numeroPersona,numeroMotivo,numeroLugar,cartasganadoras):
+    """
+    Revisa si la acusacion de un jugador es correcta.
+
+    Esta solo es correcta si el jugador acierta el culpable, el motivo y el lugar.
+
+    ∀x. (Culpable(x) ∧ Lugar(y) ∧ Motivo (z) →RespuestaCorrecta(x,y,z))
+    """
+    if numeroPersona not in cartasganadoras:
+        mensaje="Perdiste"
+        return mensaje
+    if numeroMotivo not in cartasganadoras:
+        mensaje="Perdiste"
+        return mensaje
+    if numeroLugar not in cartasganadoras:
+        mensaje="Perdiste"
+        return mensaje
+    mensaje="Ganaste"
+    return mensaje
+    
